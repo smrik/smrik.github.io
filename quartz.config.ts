feedfa2be9af8778f2c656d8c26a30803afb78b3
smrik <patrik.smid@gmail.com>
2025-10-23 23:27:44 +0200

    Quartz sync: Oct 23, 2025, 11:27 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "smrik.github.io/smrik.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
